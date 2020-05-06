71fe24995f728a1353bb319096fb707e5f31303e
hikkymouse1007 <mc.mdt1007it@gmail.com>
2020-05-07 00:20:41 +0900

    first commit

--- lambda_redirect_function.py
+++ lambda_redirect_function.py
@@ -6,7 +6,7 @@ s3 = boto3.resource('s3')      # ③S3オブジェクトを取得
  
 # ④Lambdaのメイン関数
 def lambda_handler(event, context):
-    bucket = 'msm-log-bucket'    # ⑤バケット名を指定
+    bucket = 'your-bucket-name'    # ⑤バケット名を指定
     key = 'test_' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.txt'  # ⑥オブジェクトのキー情報を指定
     file_contents = 'Lambda test'  # ⑦ファイルの内容
     
